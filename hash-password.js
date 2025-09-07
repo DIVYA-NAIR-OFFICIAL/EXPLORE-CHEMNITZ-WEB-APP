43bfd0e66e1ca04b81c02ad00de5ddd8746be7ec
ndiv-at-tu-chemnitz.de <divya-pankajakshan.nair@s2023.tu-chemnitz.de>
2025-09-07 10:35:03 +0200

    Fix Vercel deployment issues by correcting folder structure and API routes

--- hash-password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function hashPassword() {
-  const password = 'yourpassword'; // Change this to the password you want
-  const hashed = await bcrypt.hash(password, 10);
-  console.log('Hashed password:', hashed);
-}
-
-hashPassword();
